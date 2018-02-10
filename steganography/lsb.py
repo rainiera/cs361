fe39729e68693bb7c14d9eec6cb02d93fcc55f16
rainiera <rainier@utexas.edu>
2018-02-10 16:47:10 -0600

    push

--- steganography/lsb.py
+++ steganography/lsb.py
@@ -4,15 +4,15 @@ import sys
 from PIL import Image
 
 
-def encode(input_fn, secret_fn, output_fn):
-    input_im = Image.open(input_fn)
+def encode(cover_fn, secret_fn, output_fn):
+    cover_im = Image.open(cover_fn)
     secret_im = Image.open(secret_fn)
 
     # invalid input checking
-    if input_im.format != secret_im.format or input_im.mode != secret_im.mode:
+    if cover_im.format != secret_im.format or cover_im.mode != secret_im.mode:
         print('Error: Format or mode mismatch.')
         sys.exit(1)
-    if input_im.size[0] < secret_im.size[0] or input_im.size[1] < secret_im.size[1]:
+    if cover_im.size[0] < secret_im.size[0] or cover_im.size[1] < secret_im.size[1]:
         print('Error: Length or width of secret image cannot exceed length or width of image to hide it in.')
         sys.exit(1)
 
@@ -22,7 +22,20 @@ def encode(input_fn, secret_fn, output_fn):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='CLI for least-significant-bit steganography.')
-    input_fn = 'teapot.png'
-    secret_fn = 'secret.png'
-    output_fn = 'output.png'
-    encode(input_fn, secret_fn, output_fn)
+    parser.add_argument('cover_fn', action="store")
+    parser.add_argument('secret_fn', action="store")
+    parser.add_argument('num_bits', action="store", type=int)
+    parser.add_argument('output_fn', action="store", default='output.png')
+
+    args = parser.parse_args()
+    cover_fn = args.cover_fn
+    secret_fn = args.secret_fn
+    output_fn = args.output_fn
+    num_bits = args.num_bits
+    print('Using {} as cover image filename'.format(cover_fn))
+    print('Using {} as secret image filename'.format(secret_fn))
+    print('Using {} as number of least significant bits to use'.format(num_bits))
+    print('Using {} as output image filename'.format(output_fn))
+
+    encode(cover_fn, secret_fn, output_fn)
+
