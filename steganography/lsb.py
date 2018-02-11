2e201ad85750e73bb47ddb5194dbaf0bf54bb2fd
rainiera <rainier@utexas.edu>
2018-02-11 01:29:37 -0600

    finish

--- steganography/lsb.py
+++ steganography/lsb.py
@@ -1,8 +1,8 @@
 import argparse
 import sys
 
-from PIL import Image
 import numpy as np
+from PIL import Image
 
 
 def encode(cover_fn, secret_fn, output_fn, num_bits):
@@ -20,11 +20,30 @@ def encode(cover_fn, secret_fn, output_fn, num_bits):
     # do encoding here
     cover_im_array = np.array(cover_im)
     secret_im_array = np.array(secret_im)
-    output_im_array = np.zeros(cover_im_array.shape, dtype='uint8')
+    # output_im_array = np.zeros(cover_im_array.shape, dtype='uint8')
 
-    output_im_array_n_lsb_masked = cover_im_array & (256 - 2**7)
-    secret_im_array_n_msb_shifted = np.zeros(cover_im_array.shape, dtype='uint8')
+    # mask the n least significant bits
+    cover_im_array_n_lsb_masked = cover_im_array & (256 - 2**num_bits)
 
+    # secret_im_array_n_msb_shifted = np.zeros(cover_im_array.shape, dtype='uint8')
+    secret_im_array_n_msb_shifted = secret_im_array & (256 - 2**(8 - num_bits)) - 1
+
+    # pad secret image array with 0's to match cover image array dims
+    old_size = secret_im_array_n_msb_shifted.shape[:2]
+    new_size = cover_im_array.shape[:2]
+    secret_im_n_msb_shifted_padded = Image.new('RGB', list(reversed(new_size)))
+    secret_im_n_msb_shifted_padded.paste(Image.fromarray(secret_im_array_n_msb_shifted),
+        (
+            (new_size[1] - old_size[1]) / 2,
+            (new_size[0] - old_size[0]) / 2
+        )
+    )
+    # secret_im_array_n_msb_shifted_padded.show()
+    secret_im_n_msb_shifted_padded_array = np.array(secret_im_n_msb_shifted_padded)
+
+
+    output_im_array = cover_im_array_n_lsb_masked + ((secret_im_n_msb_shifted_padded_array) >> (8 - num_bits))
+    # Image.fromarray(cover_im_array_n_lsb_masked).show()
     # output_im_array[:,:100] = [255, 128, 0]
     # output_im_array[:,100:] = [0, 0, 255]
 
@@ -32,9 +51,9 @@ def encode(cover_fn, secret_fn, output_fn, num_bits):
     output_im.save(output_fn)
 
 
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='CLI for least-significant-bit steganography.')
+    parser = argparse.ArgumentParser(
+        description='CLI for least-significant-bit steganography.')
     parser.add_argument('cover_fn', action="store")
     parser.add_argument('secret_fn', action="store")
     parser.add_argument('num_bits', action="store", type=int)
@@ -52,4 +71,3 @@ if __name__ == '__main__':
     print('Using {} as output image filename'.format(output_fn))
 
     encode(cover_fn, secret_fn, output_fn, num_bits)
-
